4041cf91b8536776c2cbe407abe05be56e983b83
Vomitatrix <david.j.procop@gmail.com>
2022-07-16 19:43:50 +0200

    Added typing area

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function pressKey(e) {
+const typingArea = document.querySelector('.typing-area');
+
+function pressKey(e) { // add 'pressed' class to element that's being pressed
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.add('pressed');
 }
 
-function releaseKey(e) {
+function releaseKey(e) { // remove 'pressed' class from element that stops being pressed
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.remove('pressed');
