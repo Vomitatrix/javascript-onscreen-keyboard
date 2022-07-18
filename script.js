eed0ab49b630298ebceefc9e407a0e6c5a64abdc
Vomitatrix <david.j.procop@gmail.com>
2022-07-18 13:11:12 +0200

    Made keyboard functional

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const typingArea = document.querySelector('.typing-area');
+let input = '';
+let text = '';
 
 function pressKey(e) { // add 'pressed' class to element that's being pressed
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.add('pressed');
+    typing(e);
 }
 
 function releaseKey(e) { // remove 'pressed' class from element that stops being pressed
@@ -12,5 +15,34 @@ function releaseKey(e) { // remove 'pressed' class from element that stops being
     key.classList.remove('pressed');
 }
 
+function typing(e) {
+    let kc = e.keyCode;
+    if (kc === 8) { // backspace
+        text = text.slice(0, -1)
+    } else {
+        if (kc === 13) { // enter
+            input = '\n';
+        } else if (kc === 16) { // shift
+            input = '';
+        } else if (kc === 32) { // space
+            input = ' ';
+        } else { // letters, numbers, symbols
+            input = e.key;
+        }
+        text += input;
+    }
+    typingArea.textContent = text;    
+}
+
 window.addEventListener('keydown', pressKey);
 window.addEventListener('keyup', releaseKey);
+
+/*
+backspace 8
+enter 13
+shift 16
+space 32
+*/
+
+let asdf = 'asdf';
+const INCH = 0.0254;
