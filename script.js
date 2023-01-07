cffb995de0d03501b137e7553c42636cc9b280ee
xubuntu-laptop <david.j.procop@gmail.com>
2023-01-06 22:00:49 -0500

    Added some box and text shadows to make the keyboard look better overall

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const typingArea = document.querySelector('.typing-area');
-const letters = Array.from(document.querySelectorAll('.letter'));
-const symbols = Array.from(document.querySelectorAll('.symbol'));
-const shiftkeys = Array.from(document.querySelectorAll('.shiftkey'));
+const letters = document.querySelectorAll('.letter');
+const symbols = document.querySelectorAll('.symbol');
+const shiftkeys = document.querySelectorAll('.shiftkey');
 const upper = document.querySelector('.upper');
 let input = '';
 let text = '';
 
-function pressKey(e) { // add 'pressed' class to element that's being pressed
+function pressKey(e) {
+    // add 'pressed' class to element that's being pressed
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.add('pressed');
@@ -14,7 +15,8 @@ function pressKey(e) { // add 'pressed' class to element that's being pressed
     typing(e);
 }
 
-function releaseKey(e) { // remove 'pressed' class from element that stops being pressed
+function releaseKey(e) {
+    // remove 'pressed' class from element that stops being pressed
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.remove('pressed');
@@ -32,7 +34,7 @@ function shiftPressed() {
 }
 
 function shiftReleased() {
-    if (!(upper.classList.contains('shifted'))) {
+    if (!upper.classList.contains('shifted')) {
         letters.forEach(letter => letter.classList.remove('shift-letter'));
         symbols.forEach(symbol => symbol.classList.remove('shift-symbol'));
         shiftkeys.forEach(shiftkey => shiftkey.classList.remove('shift-shiftkey'));
@@ -41,21 +43,26 @@ function shiftReleased() {
 
 function typing(e) {
     let kc = e.keyCode;
-    if (kc === 8) { // backspace
-        text = text.slice(0, -1)
+    if (kc === 8) {
+        // backspace
+        text = text.slice(0, -1);
     } else {
-        if (kc === 13) { // enter
+        if (kc === 13) {
+            // enter
             input = '\n';
-        } else if (kc === 16) { // shift
+        } else if (kc === 16) {
+            // shift
             input = '';
-        } else if (kc === 32) { // space
+        } else if (kc === 32) {
+            // space
             input = ' ';
-        } else { // letters, numbers, symbols
+        } else {
+            // letters, numbers, symbols
             input = e.key;
         }
         text += input;
     }
-    typingArea.textContent = text;    
+    typingArea.textContent = text;
 }
 
 window.addEventListener('keydown', pressKey);
@@ -67,3 +74,8 @@ enter 13
 shift 16
 space 32
 */
+
+// key styling with JS because I could not figure it out with just CSS
+document.querySelectorAll('.key').forEach(key => {
+    key.style.minWidth = getComputedStyle(key).height;
+});
