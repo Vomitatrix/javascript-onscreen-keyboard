32010d32fbd7c2bf189855d32e00be3b96906f25
Vomitatrix <david.j.procop@gmail.com>
2023-01-20 10:11:10 +0100

    Redid the entire project with new knowledge, changed the style a little

--- script.js
+++ script.js
@@ -1,79 +1,72 @@
+'use strict';
+
 const typingArea = document.querySelector('.typing-area');
-const letters = document.querySelectorAll('.letter');
-const symbols = document.querySelectorAll('.symbol');
-const shiftkeys = document.querySelectorAll('.shiftkey');
-const upper = document.querySelector('.upper');
-let input = '';
+const keys = document.querySelectorAll('.key');
+const symbolMains = document.querySelectorAll('.symbol-main');
+const symbolSecondarys = document.querySelectorAll('.symbol-secondary');
 let text = '';
+let input = '';
 
-function pressKey(e) {
-    // add 'pressed' class to element that's being pressed
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!key) return;
-    key.classList.add('pressed');
-    if (e.keyCode === 16) shiftPressed();
-    typing(e);
+function shiftFunc() {
+    keys.forEach(key => {
+        if (key.innerHTML.length < 3) key.classList.toggle('shift-on');
+    });
+    symbolMains.forEach(key => key.classList.toggle('shift-symbol-main'));
+    symbolSecondarys.forEach(key =>
+        key.classList.toggle('shift-symbol-secondary')
+    );
 }
 
-function releaseKey(e) {
-    // remove 'pressed' class from element that stops being pressed
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!key) return;
-    key.classList.remove('pressed');
-    if (e.keyCode === 16) {
-        upper.classList.remove('shifted');
-        shiftReleased();
-    }
+function capsLock() {
+    keys.forEach(key => {
+        if (key.innerHTML.length < 3) key.classList.toggle('shift-on');
+    });
 }
 
-function shiftPressed() {
-    upper.classList.add('shifted');
-    letters.forEach(letter => letter.classList.add('shift-letter'));
-    symbols.forEach(symbol => symbol.classList.add('shift-symbol'));
-    shiftkeys.forEach(shiftkey => shiftkey.classList.add('shift-shiftkey'));
-}
+function typingFunc(keyCode, key) {
+    // do nothing if shift or caps
+    if (keyCode === 16 || keyCode === 20) return;
 
-function shiftReleased() {
-    if (!upper.classList.contains('shifted')) {
-        letters.forEach(letter => letter.classList.remove('shift-letter'));
-        symbols.forEach(symbol => symbol.classList.remove('shift-symbol'));
-        shiftkeys.forEach(shiftkey => shiftkey.classList.remove('shift-shiftkey'));
-    }
-}
-
-function typing(e) {
-    let kc = e.keyCode;
-    if (kc === 8) {
-        // backspace
+    // do what the keys are supposed to do
+    if (keyCode === 8) {
         text = text.slice(0, -1);
     } else {
-        if (kc === 13) {
-            // enter
+        if (keyCode === 13) {
             input = '\n';
-        } else if (kc === 16) {
-            // shift
-            input = '';
-        } else if (kc === 32) {
-            // space
+        } else if (keyCode === 32) {
             input = ' ';
         } else {
-            // letters, numbers, symbols
-            input = e.key;
+            input = key;
         }
         text += input;
     }
+}
+
+function keyAction(e, pressedStatus) {
+    const currKey = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    if (!currKey) return;
+
+    pressedStatus
+        ? currKey.classList.add('pressed')
+        : currKey.classList.remove('pressed');
+    if (e.keyCode === 16) shiftFunc();
+}
+
+function keyPress(e) {
+    keyAction(e, true);
+    typingFunc(e.keyCode, e.key);
+    if (e.keyCode === 20) capsLock();
     typingArea.textContent = text;
 }
 
-window.addEventListener('keydown', pressKey);
-window.addEventListener('keyup', releaseKey);
+function keyRelease(e) {
+    keyAction(e, false);
+}
 
-/*
-backspace 8
-enter 13
-shift 16
-space 32
-*/
+window.addEventListener('keydown', keyPress);
+window.addEventListener('keyup', keyRelease);
+// window.addEventListener('keydown', keyAction);
+// window.addEventListener('keyup', keyAction);
 
 // key styling with JS because I could not figure it out with just CSS
 document.querySelectorAll('.key').forEach(key => {
